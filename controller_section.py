3c927b49d715b324c6d85be5a2bdc3c871b28ad9
adamantmc <adamantiosminas@gmail.com>
2017-04-11 16:11:48 +0300

    Controller styling

--- controller_section.py
+++ controller_section.py
@@ -1,6 +1,8 @@
 from PyQt4 import QtCore, QtGui
 from new_controller_dialog import NewControllerDialog
 from custom_tableWidget import CustomTableWidget
+from custom_combobox import CustomComboBox
+from custom_lineEdit import CustomLineEdit
 from widget_util import *
 
 class ControllerSection(QtGui.QFrame):
@@ -13,7 +15,7 @@ class ControllerSection(QtGui.QFrame):
 
         #Create layouts and add content
         controller_left_layout = QtGui.QVBoxLayout(controller_left_frame)
-        controller_left_layout.addWidget(self.createControllerComboBox())
+        controller_left_layout.addWidget(self.createControllerComboBox(), 0, QtCore.Qt.AlignHCenter)
         controller_left_layout.addWidget(self.createControllerUnitTable())
 
         controller_right_layout = QtGui.QVBoxLayout(controller_right_frame)
@@ -40,7 +42,7 @@ class ControllerSection(QtGui.QFrame):
         return dialog
 
     def createControllerComboBox(self):
-        combo_box = QtGui.QComboBox()
+        combo_box = CustomComboBox()
         for i in range(2,7):
             combo_box.addItem("Fieldscale FcT520"+str(i))
 
@@ -54,6 +56,14 @@ class ControllerSection(QtGui.QFrame):
         edit_button = QtGui.QPushButton("Edit")
         delete_button = QtGui.QPushButton("Delete")
 
+        add_controller_button.setProperty("buttonType","controllerAddControllerButton")
+        edit_button.setProperty("buttonType","borderOnlyControllerButton")
+        delete_button.setProperty("buttonType","borderOnlyControllerButton")
+
+        add_controller_button.setFixedHeight(32)
+        edit_button.setFixedHeight(32)
+        delete_button.setFixedHeight(32)
+
         button_layout = QtGui.QHBoxLayout()
         button_layout.addWidget(add_controller_button)
         button_layout.addWidget(edit_button)
@@ -62,65 +72,110 @@ class ControllerSection(QtGui.QFrame):
         return button_layout
 
     def createControllerUnitTable(self):
-        unit_table = CustomTableWidget()
-        unit_table.setColumnCount(2)
-        unit_table.setRowCount(3)
+        unit_table = QtGui.QFrame()
+        unit_table.setProperty("tableType","controllerUnitTable")
+        unit_table.setSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Maximum)
+        unit_table_layout = QtGui.QVBoxLayout()
+        unit_table_layout.setContentsMargins(0,0,0,0)
 
-        unit_table.setHorizontalHeaderItem(0, createHeaderItem("Parameters"))
-        unit_table.setHorizontalHeaderItem(1, createHeaderItem("Unit (p/l)"))
+        header_frame = QtGui.QFrame()
+        header_layout = QtGui.QGridLayout()
+        parameters_label = QtGui.QLabel("Parameters")
+        unit_label = QtGui.QLabel("Unit (p/l)")
 
-        unit_table.horizontalHeader().setProperty("headerType", "tableHeader")
+        header_layout.addWidget(parameters_label,0,0)
+        header_layout.addWidget(unit_label,0,1)
 
-        unit_table.setItem(0, 0, createTableWidgetItem("Cmin"))
-        unit_table.setItem(0, 1, createTableWidgetItem("1"))
-        unit_table.setItem(1, 0, createTableWidgetItem("Cmax"))
-        unit_table.setItem(1, 1, createTableWidgetItem("2.5"))
-        unit_table.setItem(2, 0, createTableWidgetItem("Sensitivity"))
-        unit_table.setItem(2, 1, createTableWidgetItem("0.0851"))
+        header_layout.setColumnStretch(0,1)
+        header_layout.setColumnStretch(1,1)
 
-        unit_table.setShowGrid(False)
-        unit_table.verticalHeader().setVisible(False)
-        unit_table.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
+        header_frame.setLayout(header_layout)
 
-        header = unit_table.horizontalHeader()
-        header.setResizeMode(1, QtGui.QHeaderView.Stretch)
-        header.setResizeMode(2, QtGui.QHeaderView.Stretch)
+        header_frame.setProperty("rowType","unitTableHeader")
 
-        unit_table.setFixedHeight(250)
+        unit_table_layout.addWidget(header_frame)
 
+        labels = ["Cmin","Cmax","Sensitivity"]
+        values = ["1","2.5","0.0851"]
+        for i in range(3):
+            layer_frame = QtGui.QFrame(self)
+            layer_frame_layout = QtGui.QGridLayout(layer_frame)
+            layer_frame_layout.setContentsMargins(0, 0, 0, 0)
+
+            item_label = QtGui.QLabel(labels[i])
+            item_value = QtGui.QLabel(values[i])
+
+            layer_frame_layout.setColumnStretch(0, 1)
+            layer_frame_layout.setColumnStretch(1, 1)
+
+            layer_frame_layout.addWidget(item_label, 0, 0)
+            layer_frame_layout.addWidget(item_value, 0, 1)
+
+            if i%2 != 0:
+                layer_frame.setProperty("frameType", "alternateRowFrame")
+            else:
+                layer_frame.setProperty("frameType", "tableRowFrame")
+
+            unit_table_layout.addWidget(layer_frame)
+
+        unit_table.setLayout(unit_table_layout)
         return unit_table
 
     def createControllerElectrodeTable(self):
-        electrode_table = CustomTableWidget()
-        electrode_table.setColumnCount(3)
-        electrode_table.setRowCount(2)
+        electrode_table = QtGui.QFrame()
+        electrode_table.setProperty("tableType","controllerElectrodeTable")
 
-        electrode_table.setHorizontalHeaderItem(0, createHeaderItem("Electrodes"))
-        electrode_table.setHorizontalHeaderItem(1, createHeaderItem("Total"))
-        electrode_table.setHorizontalHeaderItem(2, createHeaderItem("Active"))
+        electrode_table.setSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Maximum)
+        electrode_table_layout = QtGui.QVBoxLayout()
+        electrode_table_layout.setContentsMargins(0,0,0,0)
 
-        electrode_table.setItem(0,0, createTableWidgetItem("X Electrodes"))
-        electrode_table.setItem(0,1, createTableWidgetItem("22"))
-        electrode_table.setCellWidget(0,2, self.createElectrodeTableCombo())
-        electrode_table.setItem(1,0, createTableWidgetItem("Y Electrodes"))
-        electrode_table.setItem(1,1, createTableWidgetItem("30"))
-        electrode_table.setCellWidget(1,2, self.createElectrodeTableCombo())
+        header_frame = QtGui.QFrame()
+        header_layout = QtGui.QGridLayout()
+        electrodes_label = QtGui.QLabel("Electrodes")
+        total_label = QtGui.QLabel("Total")
+        active_label = QtGui.QLabel("Active")
 
-        electrode_table.setShowGrid(False)
-        electrode_table.verticalHeader().setVisible(False)
-        electrode_table.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
-        electrode_table.horizontalHeader().setProperty("headerType", "tableHeader")
+        header_layout.addWidget(electrodes_label, 0, 0)
+        header_layout.addWidget(total_label, 0, 1)
+        header_layout.addWidget(active_label, 0, 2)
 
-        header = electrode_table.horizontalHeader()
-        header.setResizeMode(1, QtGui.QHeaderView.Stretch)
-        header.setResizeMode(2, QtGui.QHeaderView.Stretch)
+        header_layout.setColumnStretch(0,1)
+        header_layout.setColumnStretch(1,1)
+        header_layout.setColumnStretch(2,1)
 
-        electrode_table.setFixedHeight(250)
+        header_frame.setLayout(header_layout)
 
+        header_frame.setProperty("rowType","electrodeTableHeader")
+
+        electrode_table_layout.addWidget(header_frame)
+
+        labels = ["X Electrodes", "Y Electrodes"]
+        values = ["22", "30"]
+
+        for i in range(2):
+            layer_frame = QtGui.QFrame(self)
+            layer_frame_layout = QtGui.QGridLayout(layer_frame)
+            layer_frame_layout.setContentsMargins(0, 0, 0, 0)
+
+            item_label = QtGui.QLabel(labels[i])
+            item_value = QtGui.QLabel(values[i])
+
+            layer_frame_layout.setColumnStretch(0, 1)
+            layer_frame_layout.setColumnStretch(1, 1)
+            layer_frame_layout.setColumnStretch(2, 1)
+
+            layer_frame_layout.addWidget(item_label, 0, 0)
+            layer_frame_layout.addWidget(item_value, 0, 1)
+            layer_frame_layout.addWidget(self.createElectrodeTableCombo(),0,2)
+
+            layer_frame.setProperty("frameType", "tableRowFrame")
+            electrode_table_layout.addWidget(layer_frame)
+
+        electrode_table.setLayout(electrode_table_layout)
         return electrode_table
 
     def createElectrodeTableCombo(self):
-        box = QtGui.QComboBox()
+        box = CustomComboBox()
         for i in range(1, 4):
             box.addItem(str(i))
 
